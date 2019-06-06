794e217d08bb85fd639856994709484a84ab0f07
Norbert Preining <norbert@preining.info>
2019-06-07 07:26:36 +0900

    add syslog message to skill generation

--- raspi/media_daemon/auto_skills.py
+++ raspi/media_daemon/auto_skills.py
@@ -2,6 +2,7 @@ import os
 import sys
 from glob import glob
 import shutil
+import syslog
 
 # To get media_daemon folder
 if len(sys.argv) <= 1:
@@ -13,11 +14,12 @@ base_folder = os.path.dirname(os.path.dirname(os.path.dirname(media_daemon_folde
 server_skill_folder = os.path.join(base_folder, 'susi_server/data/generic_skills/media_discovery')
 
 def make_skill(): # pylint-enable
+    syslog.syslog("Generating audio skills for USB drive at " + mntpt)
     mp3_files = glob(mntpt + '/**/*.[mM][pP]3', recursive = True)
     ogg_files = glob(mntpt + '/**/*.[oO]gg', recursive = True)
     flac_files = glob(mntpt + '/**/*.flac', recursive = True)
     wav_files = glob(mntpt + '/**/*.wav', recursive = True)
-    f = open( '/home/pi/SUSI.AI/susi_installer/raspi/media_daemon/custom_skill.txt','w')
+    f = open( base_folder + '/susi_installer/raspi/media_daemon/custom_skill.txt', 'w')
     music_path = list()
     for mp in mp3_files:
         music_path.append("{}".format(mp))
@@ -30,12 +32,13 @@ def make_skill(): # pylint-enable
     # we choose ; as separation char since this seems not to be used in
     # any normal file system path naming
     song_list = ";".join( map ( lambda x: "file://" + x, music_path ) )
-    # TODO format of the skill looks strange!!!
+    # TODO add more skills
     skills = ['play audio','!console:Playing audio from your usb device','{"actions":[','{"type":"audio_play", "identifier_type":"url", "identifier":"' + str(song_list) +'"}',']}','eol']
     for skill in skills:
         f.write(skill + '\n')
     f.close()
     shutil.move(os.path.join(media_daemon_folder, 'custom_skill.txt'), os.path.join(server_skill_folder, 'custom_skill.txt'))
+    syslog.syslog("Added skills based on " + str(len(music_path)) + " songs from " + mntpt)
 
 if __name__ == '__main__':
     make_skill()
