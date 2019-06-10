add07cd9ad4d537d7dedf1f1c1642f64eba28e5e
alok <alok760@gmail.com>
2019-06-11 04:22:53 +0530

    use mutagen to parse metadata and handle all formats

--- raspi/media_daemon/auto_skills.py
+++ raspi/media_daemon/auto_skills.py
@@ -3,7 +3,7 @@ import sys
 from glob import glob
 import shutil
 import syslog
-import eyed3
+import mutagen
 
 # To get media_daemon folder
 if len(sys.argv) <= 1:
@@ -23,16 +23,15 @@ def make_skill(): # pylint-enable
     f = open( base_folder + '/susi_installer/raspi/media_daemon/custom_skill.txt', 'w')
     music_path = list()
     artists={}
-    for mp in mp3_files[:]:
-        music_path.append("{}".format(mp))
-        audiofile = eyed3.load(mp)
-        artists.setdefault(audiofile.tag.artist, []).append(mp)
-    for ogg in ogg_files:
-        music_path.append("{}".format(ogg))
-    for flac in flac_files:
-        music_path.append("{}".format(flac))
-    for wav in wav_files:
-        music_path.append("{}".format(wav))
+    for audiofiles in mp3_files,ogg_files,flac_files, wav_files:
+        for audiofile in audiofiles:
+            music_path.append("{}".format(audiofile))
+            mdata = mutagen.File(audiofile)
+            if mdata!=None:
+                if 'TPE1' in mdata:
+                    artists.setdefault(mdata['TPE1'][0], []).append(audiofile)
+                elif 'artist' in mdata:
+                    artists.setdefault(str(mdata['artist'][0]), []).append(audiofile)
     # we choose ; as separation char since this seems not to be used in
     # any normal file system path naming
     song_list = ";".join( map ( lambda x: "file://" + x, music_path ) )
