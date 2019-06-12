807081e2b78b79021279507c0452f8b18e0bdf88
Norbert Preining <norbert@preining.info>
2019-06-12 16:10:57 +0900

    switch to POST and PUT method instead of GET (#32)
    * switch to POST and PUT method instead of GET
    
    * fix broken status route
    
    - http return code was wrong
    - add PUT/POST

--- soundserver/soundserver.py
+++ soundserver/soundserver.py
@@ -20,13 +20,13 @@ def do_return(msg, val):
 def index():
     return render_template('index.html')
 
-@app.route('/status')
+@app.route('/status', methods=['POST', 'PUT'])
 def status_route():
-    return do_return('Ok', 400)
+    return do_return('Ok', 200)
 
 # /play?ytb=???
 # /play?mrl=???
-@app.route('/play', methods=['GET'])
+@app.route('/play', methods=['POST', 'PUT'])
 def play_route():
     if 'ytb' in request.args:
         vlcplayer.playytb(request.args.get('ytb'))
@@ -40,7 +40,7 @@ def play_route():
 # /volume?val=up
 # /volume?val=down
 # /volume?val=NN  0 <= NN <= 100
-@app.route('/volume', methods=['GET'])
+@app.route('/volume', methods=['POST', 'PUT'])
 def volume_route():
     try:
         vlcplayer.volume(request.args.get('val'))
@@ -50,14 +50,14 @@ def volume_route():
         return do_return('Volume adjustment error' + e, 400)
 
 # /say?mrl=???
-@app.route('/say', methods=['GET'])
+@app.route('/say', methods=['POST', 'PUT'])
 def say_route():
     if 'mrl' in request.args:
         vlcplayer.say(request.args.get('mrl'))
         return do_return('Ok', 200)
     else:
         return do_return('Missing mrl argument', 400)
-@app.route('/beep', methods=['GET'])
+@app.route('/beep', methods=['POST', 'PUT'])
 def beep_route():
     if 'mrl' in request.args:
         vlcplayer.beep(request.args.get('mrl'))
@@ -69,53 +69,53 @@ def beep_route():
 # /pause
 # /resume
 # /stop
-@app.route('/pause', methods=['GET'])
+@app.route('/pause', methods=['POST', 'PUT'])
 def pause_route():
     vlcplayer.pause()
     return do_return('Ok', 200)
-@app.route('/resume', methods=['GET'])
+@app.route('/resume', methods=['POST', 'PUT'])
 def resume_route():
     vlcplayer.resume()
     return do_return('Ok', 200)
-@app.route('/stop', methods=['GET'])
+@app.route('/stop', methods=['POST', 'PUT'])
 def stop_route():
     vlcplayer.stop()
     return do_return('Ok', 200)
 
-@app.route('/next', methods=['GET'])
+@app.route('/next', methods=['POST', 'PUT'])
 def next_route():
     vlcplayer.next()
     return do_return('Ok', 200)
 
-@app.route('/previous', methods=['GET'])
+@app.route('/previous', methods=['POST', 'PUT'])
 def previous_route():
     vlcplayer.previous()
     return do_return('Ok', 200)
 
-@app.route('/restart', methods=['GET'])
+@app.route('/restart', methods=['POST', 'PUT'])
 def restart_route():
     vlcplayer.restart()
     return do_return('Ok', 200)
 
-@app.route('/shuffle', methods=['GET'])
+@app.route('/shuffle', methods=['POST', 'PUT'])
 def shuffle_route():
     vlcplayer.shuffle()
     return do_return('Ok', 200)
 
-@app.route('/save_softvolume', methods=['GET'])
+@app.route('/save_softvolume', methods=['POST', 'PUT'])
 def save_softvolume_route():
     vlcplayer.save_softvolume()
     return do_return('Ok', 200)
-@app.route('/restore_softvolume', methods=['GET'])
+@app.route('/restore_softvolume', methods=['POST', 'PUT'])
 def restore_softvolume_route():
     vlcplayer.restore_softvolume()
     return do_return('Ok', 200)
 
-@app.route('/save_hardvolume', methods=['GET'])
+@app.route('/save_hardvolume', methods=['POST', 'PUT'])
 def save_hardvolume_route():
     vlcplayer.save_hardvolume()
     return do_return('Ok', 200)
-@app.route('/restore_hardvolume', methods=['GET'])
+@app.route('/restore_hardvolume', methods=['POST', 'PUT'])
 def restore_hardvolume_route():
     vlcplayer.restore_hardvolume()
     return do_return('Ok', 200)
