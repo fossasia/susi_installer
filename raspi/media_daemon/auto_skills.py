1998cd0c9cd679f1a07859c0c6b3f2fac65c049d
Alok Kumar <alok760@gmail.com>
2019-06-14 09:19:30 +0530

    Add feature to play songs from a particular artist from the USB device (#29)
    - partially tackle Issue 28
    - create skills for soung artists "play XXX from usb"
    - based on mutagen

--- raspi/media_daemon/auto_skills.py
+++ raspi/media_daemon/auto_skills.py
@@ -3,6 +3,7 @@ import sys
 from glob import glob
 import shutil
 import syslog
+import mutagen
 
 # To get media_daemon folder
 if len(sys.argv) <= 1:
@@ -21,22 +22,32 @@ def make_skill(): # pylint-enable
     wav_files = glob(mntpt + '/**/*.wav', recursive = True)
     f = open( base_folder + '/susi_installer/raspi/media_daemon/custom_skill.txt', 'w')
     music_path = list()
-    for mp in mp3_files:
-        music_path.append("{}".format(mp))
-    for ogg in ogg_files:
-        music_path.append("{}".format(ogg))
-    for flac in flac_files:
-        music_path.append("{}".format(flac))
-    for wav in wav_files:
-        music_path.append("{}".format(wav))
+    artists={}
+    for audiofiles in mp3_files,ogg_files,flac_files, wav_files:
+        for audiofile in audiofiles:
+            music_path.append("{}".format(audiofile))
+            mdata = mutagen.File(audiofile)
+            if mdata!=None:
+                if 'TPE1' in mdata:
+                    artists.setdefault(mdata['TPE1'][0], []).append(audiofile)
+                elif 'artist' in mdata:
+                    artists.setdefault(str(mdata['artist'][0]), []).append(audiofile)
     # we choose ; as separation char since this seems not to be used in
     # any normal file system path naming
     song_list = ";".join( map ( lambda x: "file://" + x, music_path ) )
-    # TODO add more skills
     skills = ['play audio','!console:Playing audio from your usb device','{"actions":[','{"type":"audio_play", "identifier_type":"url", "identifier":"' + str(song_list) +'"}',']}','eol']
     for skill in skills:
         f.write(skill + '\n')
+    f.write("\n")
+
+    for artist_name in artists:
+        song_list = ";".join( map ( lambda x: "file://" + x, artists[artist_name] ) )
+        skills_artist = ['play '+ artist_name +' from usb','!console:Playing audio from your usb device','{"actions":[','{"type":"audio_play", "identifier_type":"url", "identifier":"' + str(song_list) +'"}',']}','eol']
+        for skill in skills_artist:
+            f.write(skill + '\n')
+        f.write("\n")
     f.close()
+
     shutil.move(os.path.join(media_daemon_folder, 'custom_skill.txt'), os.path.join(server_skill_folder, 'custom_skill.txt'))
     syslog.syslog("Added skills based on " + str(len(music_path)) + " songs from " + mntpt)
 
