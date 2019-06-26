59373e761070240d9067844d008d5a1a6f52d5c9
Norbert Preining <norbert@preining.info>
2019-06-26 11:17:46 +0900

    New update_check code
    - adapt to new layout in SUSI.AI directory
    - only pull if the current branch is master

--- raspi/update_daemon/update_check.sh
+++ raspi/update_daemon/update_check.sh
@@ -1,25 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 # To be configured at bootup
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-CHECK=''
-if [ $LOCAL = $REMOTE ]
-then
+cd /home/pi/SUSI.AI
+
+update_repo() {
+  if [ ! -d "$1" ] ; then
+    echo "Unknown repository $1" >&2
+    exit 1
+  fi
+  cd "$1"
+  # get current branch name
+  CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+  if [ ! "x$CURRENTBRANCH" = "xmaster" ] ; then
+    echo "Current branch of $1 is $CURRENTBRANCH, not master." >&2
+    echo "Not updating!"
+    return
+  fi
+  UPSTREAM=${2:-'@{u}'}
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse "$UPSTREAM")
+  BASE=$(git merge-base @ "$UPSTREAM")
+  CHECK=''
+  if [ $LOCAL = $REMOTE ]
+  then
     echo "Up-to-date"
     CHECK='up-to-date'
-elif [ $LOCAL = $BASE ] 
-then
+  elif [ $LOCAL = $BASE ] 
+  then
     echo "Need to pull"
     CHECK='Need-to-pull'
-else
+  else
     echo "Diverged"
-fi
+  fi
 
-if [$CHECK = "Need-to-pull"]
-then
-    git fetch UPSTREAM
-    git merge UPSTREAM/master
-fi
+  if [ $CHECK = "Need-to-pull" ]
+  then
+    git pull
+  fi
+  cd ..
+}
+
+update_repo susi_linux
+update_repo susi_python
+update_repo susi_installer
+
+#
+# TODO we need to think about how to handle susi_server update!
+
