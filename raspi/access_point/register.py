3c507ad29fda9478eeb6c60039003d176f293fef
alok <alok760@gmail.com>
2019-07-19 02:59:31 +0530

    get location of device

--- raspi/access_point/register.py
+++ raspi/access_point/register.py
@@ -5,6 +5,7 @@ import time
 
 import requests
 import json_config
+import geocoder
 
 def get_token(login,password):
     url = 'http://api.susi.ai/aaa/login.json?type=access-token'
@@ -16,18 +17,23 @@ def get_token(login,password):
     return r1['access_token']
 
 def device_register(access_token):
-    url='https://api.susi.ai/aaa/addNewDevice.json?&macid=21:12:23:34:12:23&name=meow&room=home&latitude=12.1&longitude=23.3'
+    g = geocoder.ip('me')
+    url='https://api.susi.ai/aaa/addNewDevice.json?&name=SmartSpeaker'
     PARAMS = {
+        'room':'home',
+        'latitude':str(g.lat),
+        'longitude':str(g.lng),
+        'macid':'21:12:23:34:12:23',
         'access_token':access_token
     }
+    print(PARAMS)
     r1 = requests.get(url, params=PARAMS).json()
     return r1
+
 config = json_config.connect('/home/pi/SUSI.AI/config.json')
 user = config['login_credentials']['email']
 password = config['login_credentials']['password']
 
-
-print("hello")
 try:
     access_token=get_token(user,password)
     print(device_register(access_token))
