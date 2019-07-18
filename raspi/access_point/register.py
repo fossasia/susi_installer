53151ab5d4566572b7a922b8f7c136f428590e9f
alok <alok760@gmail.com>
2019-07-18 10:39:59 +0530

    device registration

--- raspi/access_point/register.py
+++ raspi/access_point/register.py
@@ -1,19 +1,37 @@
-import sys
-import requests
-"""
-This script is used to register the device after it has been setup. This script should perform the given function:
-After configuration of the device is done through android app or manually, the SUSI.AI smart device will then shut down the hotspot and connect with your WiFi home network and online account.
-If SUSI.AI is not able to connect to your home WiFi network after 3 times, it will restart the SUSI.AI hotspot again and you can start the setup process again.
-If SUSI.AI connects successfully with the Internet and your account, it will show up in your list of connected devices and you can continue to configure its settings.
-"""
-#Get access token
-url = 'https://api.susi.ai/aaa/login.json?'
-PARAMS = {
-    'login':sys.argv[1],
-    'password':sys.argv[2],
-    'type':'access-token'
-}
-r1 = requests.get(url, params=PARAMS).json()
+#!/usr/bin/env python3
 
-#r2 = requests.get('https://api.susi.ai/aaa/addNewDevice.json?access_token=u8Sa26QGBGmxZOmc9Ew0WHN8FdcrL1&macid=21:12:23:34:12:23&name=meow&room=home&latitude=12.1&longitude=23.3')
-print(r1['access_token'])
+import sys
+import time
+
+import requests
+import json_config
+
+def get_token(login,password):
+    url = 'http://api.susi.ai/aaa/login.json?type=access-token'
+    PARAMS = {
+        'login':login,
+        'password':password,
+    }
+    r1 = requests.get(url, params=PARAMS).json()
+    return r1['access_token']
+
+def device_register(access_token):
+    url='https://api.susi.ai/aaa/addNewDevice.json?&macid=21:12:23:34:12:23&name=meow&room=home&latitude=12.1&longitude=23.3'
+    PARAMS = {
+        'access_token':access_token
+    }
+    r1 = requests.get(url, params=PARAMS).json()
+    return r1
+config = json_config.connect('/home/pi/SUSI.AI/config.json')
+user = config['login_credentials']['email']
+password = config['login_credentials']['password']
+
+
+print("hello")
+try:
+    access_token=get_token(user,password)
+    print(device_register(access_token))
+    print(access_token)
+except:
+    time.sleep(5)
+    print("retrying")
