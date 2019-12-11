089b8956476405043e3c25b865b5027afbe46283
Norbert Preining <norbert@preining.info>
2019-12-11 19:39:30 +0900

    install-dependencies completion

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,14 @@
-#!/bin/bash -e
-set -uo pipefail
+#!/bin/bash
+# SUSI.AI Smart Assistant dependency installer script
+# Copyright 2019 Norbert Preining
+# 
+set -euo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 PROGS="git wget sox java vlc flac python3 pip3"
 
 TRUSTPIP=0
-CLEAN=0
+CLEAN=1
 BRANCH=development
 RASPI=0
 SUDOCMD=sudo
@@ -15,8 +18,8 @@ do
     case $key in
         --trust-pip)
             TRUSTPIP=1 ; shift ;;
-        --clean)
-            CLEAN=1 ; shift ;;
+        --no-clean)
+            CLEAN=0 ; shift ;;
         --raspi)
             RASPI=1 ; shift ;;
         --sudo-cmd)
@@ -32,7 +35,7 @@ Possible options:
   --branch BRANCH  Use branch BRANCH to get requirement files (default: development)
   --raspi          Do additional installation tasks for the SUSI.AI Smart Speaker
   --sudo-cmd CMD   Use CMD instead of the default sudo
-  --clean          Use --no-cache-dir with pip3
+  --no-clean       Don't remove temp directory and don't use --no-cache-dir with pip3
 
 EOF
             exit 0
@@ -91,10 +94,10 @@ fi
 
 PIP=pip3
 if [ $CLEAN = 1 ] ; then
-    PIP="pip3 --no-cache-dir"
+    PIP="pip3 --no-cache-dir -q"
 fi
 
-if [ $UPDATEPIP=1 ] ; then
+if [ $UPDATEPIP = 1 ] ; then
     $SUDOCMD $PIP install -U pip
 fi
 
@@ -106,311 +109,36 @@ reqs="
     susi_installer:requirements-optional.txt
 "
 reqspi="
-    susi_linux.git/requirements-rpi.txt
+    susi_linux:requirements-rpi.txt
 "
 
+if [ $RASPI = 1 ] ; then
+    reqs="$reqs $reqspi"
+fi
 
+# Create temp dir
+tmpdir=$(mktemp -d)
+
+# Download requirement files
 for i in $reqs ; do
     p=$(echo $i | sed -e s+:+/$BRANCH/+)
-    wget -O $i https://raw.githubusercontent.com/fossasia/$p
+    wget -q -O $tmpdir/$i https://raw.githubusercontent.com/fossasia/$p
 done
 
-
-for i in $reqfiles ; do
-    $SUDOCMD $PIP install -r $i
+# Install pips
+for i in $reqs ; do
+    $SUDOCMD $PIP install -r $tmpdir/$i
 done
 
-
-
-
-######### UNFINISHED
-
-echo "UNFINISHED BUSINESS" >&2
-exit 1
-
-
-
-
-#    ret=`pip3 show snowboy || true`
-#    if [ -z "$ret" ] ; then
-#        install_snowboy
-#    fi
-
-# function to update the latest vlc drivers which will allow it to play MRL of latest videos
-# Only do this on old systems (stretch etc)
-if [[ ( $targetSystem = debian && $targetVersion = 9 ) \
-      || \
-      ( $targetSystem = ubuntu && $targetVersion = 18.04 ) \
-      || \
-      ( $targetSystem = mint ) \
-      || \
-      ( $targetSystem = raspi && $targetVersion = 9 ) \
-   ]]  ; then
-    wget https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua
-    echo "Updating VLC drivers"
-    ask_for_sudo
-    if [ -d /usr/lib/$HOSTARCHTRIPLE/vlc/lua/playlist/ ] ; then
-        $SUDOCMD mv youtube.lua /usr/lib/$HOSTARCHTRIPLE/vlc/lua/playlist/youtube.luac
-    else
-        echo "Cannot find directory /usr/lib/$HOSTARCHTRIPLE/vlc/lua/playlist/ - not updating youtube.lua" >&2
-    fi
-    rm -f youtube.lua
+# cleanup
+if [ $CLEAN = 1 ] ; then
+    for i in $reqs ; do
+        rm $tmpdir/$i
+    done
+    rmdir $tmpdir
 fi
 
-#
-# Add coral if selected
-#
-if [ $CORAL = 1 ] ; then
-    install_coral
-fi
+echo "Finished."
 
-#
-# install seeed card driver only on RPi
-if [ $targetSystem = raspi ]
-then
-    install_seeed_voicecard_driver
-fi
-
-# install Etherpad on RPi, including the depending modules
-if [ $targetSystem = raspi ]
-then
-    echo "Downloading: Etherpad-lite"
-    cd "$DESTDIR"
-    if [ ! -d etherpad-lite ] ; then
-        git clone --branch master https://github.com/ether/etherpad-lite.git
-    else
-        echo "WARNING: etherpad-lite directory already present, not cloning it!" >&2
-    fi
-    echo "Adding node.js repository"
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-    sudo apt-get install --no-install-recommends -y nodejs
-    echo "Installing node modules for etherpad"
-    cd etherpad-lite
-    bin/installDeps.sh
-    cd ..
-    # systemd file is automatically installed using Deploy/auto... further below
-fi
-
-if [ ! -f "susi_linux/extras/cmu_us_slt.flitevox" ]
-then
-    echo "Downloading Speech Data for flite TTS"
-    wget "http://www.festvox.org/flite/packed/flite-2.0/voices/cmu_us_slt.flitevox" -P susi_linux/extras
-fi
-
-#
-# Use pkg-config to get correct systemd install path
-#   system units: pkg-config systemd --variable=systemdsystemunitdir
-#                 on Debian: /lib/systemd/system
-#   user units:   pkg-config systemd --variable=systemduserunitdir
-#                 on Debian: /usr/lib/systemd/user
-# but install path into $HOME are fixed I guess
-systemdsystem=""
-systemduser=""
-if [ -x "$(command -v pkg-config)" ]
-then
-    systemdsystem=$(pkg-config systemd --variable=systemdsystemunitdir 2>/dev/null)
-    systemduser=$(pkg-config systemd --variable=systemduserunitdir 2>/dev/null)
-fi
-if [ -z "$systemdsystem" ] ; then
-    systemdsystem=/lib/systemd/system
-fi
-if [ -z "$systemduser" ] ; then
-    systemduser=/usr/lib/systemd/user
-fi
-systemdhomeuser=$HOME/.config/systemd/user
-
-
-if [ $targetSystem = raspi ]
-then
-    echo "Preparing USB automount"
-    # systemd-udevd creates its own filesystem namespace, so mount is done, but it is not visible in the principal namespace.
-    sudo mkdir /etc/systemd/system/systemd-udevd.service.d/
-    echo -e "[Service]\nPrivateMounts=no" | sudo tee /etc/systemd/system/systemd-udevd.service.d/udev-service-override.conf
-    # readonly mount for external USB drives
-    sudo sed -i -e '/^MOUNTOPTIONS/ s/sync/ro/' /etc/usbmount/usbmount.conf
-    sudo cp $INSTALLERDIR/raspi/media_daemon/01_create_skill /etc/usbmount/mount.d/
-    sudo cp $INSTALLERDIR/raspi/media_daemon/01_remove_auto_skill /etc/usbmount/umount.d/
-
-    echo "Installing RPi specific Systemd Rules"
-    # TODO !!! we need to make the vlcplayer available to controlserver, as of now it does not find it
-    sudo cp $INSTALLERDIR/raspi/systemd/ss-*.service $systemdsystem
-    sudo cp $INSTALLERDIR/raspi/systemd/ss-*.timer $systemdsystem
-    sudo systemctl enable ss-update-daemon.service
-    sudo systemctl enable ss-update-daemon.timer
-    sudo systemctl enable ss-factory-daemon.service
-    sudo systemctl enable ss-controlserver.service
-fi
-
-echo "Updating Susi Linux Systemd service file"
-cd "$DESTDIR"
-cp 'susi_linux/systemd/ss-susi-linux@.service.in' 'ss-susi-linux@.service'
-cp 'susi_linux/systemd/ss-susi-linux.service.in' 'ss-susi-linux.service'
-sed -i -e "s!@BINDIR@!$BINDIR!" ss-susi-linux.service
-sed -i -e "s!@BINDIR@!$BINDIR!" 'ss-susi-linux@.service'
-if [ $targetSystem = raspi -o $INSTALLMODE = user ] ; then
-    # on RasPi, we install the system units into the system directories
-    if [ $targetSystem = raspi ] ; then
-        sudo cp 'ss-susi-linux@.service' $systemdsystem
-    else
-        # Desktop in user mode
-        mkdir -p $systemdhomeuser
-        cp ss-susi-linux.service $systemdhomeuser
-    fi
-else
-    $SUDOCMD cp 'ss-susi-linux@.service' $systemdsystem
-    $SUDOCMD cp ss-susi-linux.service $systemduser
-fi
-rm 'ss-susi-linux@.service'
-rm ss-susi-linux.service
-
-echo "Installing Susi Linux Server Systemd service file"
-cd "$DESTDIR"
-cp 'susi_server/systemd/ss-susi-server.service.in' 'ss-susi-server.service'
-sed -i -e "s!@INSTALL_DIR@!$DESTDIR/susi_server!" ss-susi-server.service
-sed -i -e "s!@SUSI_SERVER_USER@!$SUSI_SERVER_USER!" ss-susi-server.service
-if [ $targetSystem = raspi -o $INSTALLMODE = user ] ; then
-    # on RasPi, we install the system units into the system directories
-    if [ $targetSystem = raspi ] ; then
-        sudo cp 'ss-susi-server.service' $systemdsystem
-        sudo systemctl daemon-reload || true
-    else
-        # Desktop in user mode
-        mkdir -p $systemdhomeuser
-        # we need to filter out the User= line from user units!
-        grep -v '^User=' ss-susi-server.service > $systemdhomeuser/ss-susi-server.service
-        systemctl --user daemon-reload || true
-    fi
-else
-    # susi-server does not support multi-user functionality by now
-    # since data/log dirs are shared
-    # $SUDOCMD cp ss-susi-server.service $systemduser
-    #
-    # add a new user for susi-server
-    $SUDOCMD adduser --system \
-            --quiet \
-            --home /nonexistent \
-            --no-create-home \
-            --disabled-password \
-            --group \
-            --force-badname \
-            $SUSI_SERVER_USER
-    $SUDOCMD mkdir -p /var/lib/susi-server/data
-    $SUDOCMD chown $SUSI_SERVER_USER:$SUSI_SERVER_USER /var/lib/susi-server/data
-    $SUDOCMD ln -s /var/lib/susi-server/data susi_server/data
-    $SUDOCMD cp ss-susi-server.service $systemdsystem
-    $SUDOCMD systemctl daemon-reload || true
-fi
-rm ss-susi-server.service
-
-sed -i -e 's/^local\.openBrowser\.enable\s*=.*/local.openBrowser.enable = false/' $DESTDIR/susi_server/conf/config.properties
-
-# enable the client service ONLY on Desktop, NOT on RPi
-# On raspi we do other setups like reset folder etc
-if [ $targetSystem = raspi ] ; then
-    # make sure that the susi_server does not open the browser on startup
-
-    # update hostname to "susi" (and not raspberrypi)
-    echo "susi" | sudo tee /etc/hostname
-
-    # enable the server service unconditionally
-    sudo systemctl enable ss-susi-server
-    sudo systemctl enable ss-etherpad-lite
-
-    # we need UTF8 char encoding, otherwise files with UTF8 names cannot
-    # be dealt with in Python
-    echo "Setting default locale to en_GB.UTF8"
-    echo "LC_ALL=en_GB.UTF8" | sudo tee -a /etc/environment
-
-    echo "Enabling the SSH access"
-    sudo systemctl enable ssh
-
-    # we need to restart udev once after reboot to get rw filesystems
-    # see:
-    # - https://github.com/raspberrypi/linux/issues/2497
-    # - https://unix.stackexchange.com/questions/401394/udev-rule-triggers-but-any-run-command-fails
-    # - https://www.raspberrypi.org/forums/viewtopic.php?t=210243
-    # this is a recent change from udev somewhen in 2018?
-    echo "Working around broken udev and ro file systems"
-    sudo mkdir -p /etc/systemd/system
-    sudo cp $INSTALLERDIR/raspi/media_daemon/udev-restart-after-boot.service /etc/systemd/system/udev-restart-after-boot.service
-    sudo systemctl enable udev-restart-after-boot
-
-    echo "Disable dhcpcd"
-    sudo systemctl disable dhcpcd
-
-    # link etherpad database file to $WORKDIR
-    touch $WORKDIR/etherpad.db
-    ln -s $WORKDIR/etherpad.db $DESTDIR/etherpad-lite/var/dirty.db
-
-    # save susi_linux server data outside of server dir
-    mkdir $WORKDIR/susi_server_data
-    ln -s $WORKDIR/susi_server_data $DESTDIR/susi_server/data
-
-    # create empty custom_skill file and make susi server read it
-    mkdir -p $WORKDIR/susi_server_data/generic_skills/media_discovery
-    touch $WORKDIR/susi_server_data/generic_skills/media_discovery/custom_skill.txt
-    mkdir -p $WORKDIR/susi_server_data/settings
-    echo "local.mode = true" > $WORKDIR/susi_server_data/settings/customized_config.properties
-
-    cd "$DESTDIR"
-    echo "Creating a backup folder for future factory_reset"
-    tar -I 'pixz -p 2' -cf ../reset_folder.tar.xz --checkpoint=.1000 --exclude susi_server_data --exclude etherpad.db --exclude config.json .
-    echo ""  # To add newline after tar's last checkpoint
-    mv ../reset_folder.tar.xz susi_installer/raspi/factory_reset/reset_folder.tar.xz
-
-    # Avahi has bug with IPv6, and make it fail to propage mDNS domain.
-    sudo sed -i 's/use-ipv6=yes/use-ipv6=no/g' /etc/avahi/avahi-daemon.conf || true
-
-    # install wlan config files: files with . in the name are *NOT* include
-    # into the global /etc/network/interfaces file, so we can keep them there.
-    echo "Installing ETH/WLAN device configuration files"
-    sudo cp $INSTALLERDIR/raspi/access_point/interfaces.d/* /etc/network/interfaces.d/
-
-    echo "Converting RasPi into an Access Point"
-    sudo bash $INSTALLERDIR/raspi/access_point/wap.sh
-
-    # install our home compiled package of libportaudio2
-    sudo dpkg -i $INSTALLERDIR/raspi/debs/libportaudio2_19.6.0-1.1_armhf.deb
-fi
-
-
-#
-# Final output
-if [ ! $targetSystem = raspi ] ; then
-    echo ""
-    echo "SUSI AI has been installed into $DESTDIR."
-    if [ $INSTALLMODE = user ] ; then
-        echo "To start it once, type"
-        echo "  systemctl --user start ss-susi-server"
-        echo "  systemctl --user start ss-susi-linux"
-        echo "To enable it permanently, use"
-        echo "  systemctl --user enable ss-susi-server"
-        echo "  systemctl --user enable ss-susi-linux"
-    else
-        echo "To start it once, type"
-        echo "  sudo systemctl start ss-susi-server"
-        echo "  systemctl --user start ss-susi-linux"
-        echo "To enable it permanently, use"
-        echo "  sudo systemctl enable ss-susi-server"
-        echo "  systemctl --user enable ss-susi-linux"
-        echo ""
-        echo "There is also a ss-susi-linux@.service file which can be"
-        echo "started/enabled for specific users via"
-        echo "  sudo systemctl start/enable ss-susi-linux@USER"
-        echo "instead of using the --user variant from above."
-    fi
-    echo ""
-    echo "Enjoy."
-else
-    echo -e "SUSI is installed successfully!"
-    echo -e "Please add $BINDIR to your PATH, then"
-    echo -e "Run configuration script by 'susi-config set stt=<stt engine> tts=<tts engine> hotword=<snowboy> or pocketsphinx> wakebutton=<wake button>'"
-    echo "For example, to configure SUSI as following: "
-    echo -e "\t - Google for speech-to-text"
-    echo -e "\t - Google for text-to-speech"
-    echo -e "\t - Use snowboy for hot-word detection"
-    echo -e "\t - Do not use GPIO for wake button"
-    echo -e "susi-config set stt=google tts=google hotword=SnowBoy wakebutton=disable"
-fi
 
 # vim: set expandtab shiftwidth=4 softtabstop=4 smarttab:
