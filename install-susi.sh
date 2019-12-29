45f7419d73892cb375232e3d96f14b7c3c841633
Norbert Preining <norbert@preining.info>
2019-12-30 08:55:31 +0900

    reseeed installer is not happy with set -e

--- install-susi.sh
+++ install-susi.sh
@@ -398,8 +398,7 @@ function install_seeed_voicecard_driver()
     # and is not necessary
     sed -i -e 's/apt-get -y install \(.*\) libasound2-plugins/apt-get -y install \1/g' install.sh
     # This happens *ONLY* on the RPi, so we can do sudo!
-    # Use -e so that errors occuring in the script are caught
-    sudo bash -e ./install.sh
+    sudo bash ./install.sh
     cd ..
     tar -czf ~/seeed-voicecard.tar.gz seeed-voicecard
     rm -rf seeed-voicecard
