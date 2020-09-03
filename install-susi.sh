5a29735d4df7e96a958d1317cf1f1827d75d8c00
Norbert Preining <norbert@preining.info>
2020-09-03 20:29:23 +0900

    fix install-susi calling itself

--- install-susi.sh
+++ install-susi.sh
@@ -336,7 +336,7 @@ if [ ! -d "raspi" ] ; then
     git clone https://github.com/fossasia/susi_installer.git
     cd susi_installer
     git checkout $SUSI_INSTALLER_BRANCH
-    exec ./install.sh $saved_args
+    exec ./install-susi.sh $saved_args
 fi
 
 
